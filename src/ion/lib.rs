d9d2c6ffd8aa9799e76d6f0b02b26fce2c87e9f6
Roger-luo <rogerluo.rl18@gmail.com>
2023-01-23 11:55:02 -0500

    re-organize spec

--- src/ion/lib.rs
+++ src/ion/lib.rs
@@ -1,13 +1,9 @@
 pub mod blueprints;
 pub mod errors;
-pub mod pkgspec;
-pub mod project;
-pub mod registry;
+pub mod spec;
 pub mod release;
 pub mod utils;
 
 pub use blueprints::{Context, Template};
 pub use errors::{CliError, CliResult};
-pub use pkgspec::PackageSpec;
-pub use project::JuliaProject;
-pub use registry::{Registry, RegistryList};
+pub use spec::*;
---
+++ src/ion/spec/mod.rs
@@ -0,0 +1,7 @@
+pub mod pkgspec;
+pub mod project;
+pub mod registry;
+
+pub use pkgspec::PackageSpec;
+pub use project::JuliaProject;
+pub use registry::{Registry, RegistryList};
