851806653406b9b307b741ced4d657486aa9032e
Roger-luo <rogerluo.rl18@gmail.com>
2023-01-26 14:52:52 -0500

    add value hints for pkg cmds

--- src/bin/ion/commands/pkg/add.rs
+++ src/bin/ion/commands/pkg/add.rs
@@ -1,13 +1,13 @@
 use crate::commands::pkg::package_spec_list;
 use clap::parser::ArgMatches;
-use clap::{arg, Command};
+use clap::{arg, Command, ValueHint};
 use ion::errors::CliResult;
 use ion::utils::Julia;
 
 pub fn cli() -> Command {
     Command::new("add")
         .about("Add dependencies to current environment")
-        .arg(arg!([PACKAGE] ... "The package to add"))
+        .arg(arg!([PACKAGE] ... "The package to add").value_hint(ValueHint::AnyPath))
         .arg_required_else_help(true)
 }
 
--- src/bin/ion/commands/pkg/develop.rs
+++ src/bin/ion/commands/pkg/develop.rs
@@ -1,6 +1,6 @@
 use crate::commands::pkg::package_spec_list;
 use clap::parser::ArgMatches;
-use clap::{arg, Command};
+use clap::{arg, Command, ValueHint};
 use ion::errors::CliResult;
 use ion::utils::Julia;
 
@@ -8,7 +8,7 @@ pub fn cli() -> Command {
     Command::new("develop")
         .visible_alias("dev")
         .about("Develop packages in the current environment")
-        .arg(arg!([PACKAGE] "The package path to develop"))
+        .arg(arg!([PACKAGE] "The package path to develop").value_hint(ValueHint::AnyPath))
         .arg(arg!(-v --verbose "show detailed output"))
         .arg(arg!(--all "garbage collect all packages which can not \
             be immediately reached from existing project"))
