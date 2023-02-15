57daf308963bebd1043ceea670930f5a2018e3f8
Roger-luo <rogerluo.rl18@gmail.com>
2023-02-15 13:10:34 -0500

    fix: run fmt

--- julia-semver/src/spec/range.rs
+++ julia-semver/src/spec/range.rs
@@ -2,8 +2,8 @@ use std::fmt::Display;
 
 use super::Version;
 use super::VersionBound;
-use anyhow::Result;
 use anyhow::anyhow;
+use anyhow::Result;
 use regex::Regex;
 
 pub struct VersionRange {
@@ -21,23 +21,26 @@ impl VersionRange {
         let version = "v?([0-9]+?)(?:\\.([0-9]+?))?(?:\\.([0-9]+?))?";
         let semver = Regex::new(format!("^([~^]?)?{version}$").as_str()).unwrap();
         let inequality = Regex::new(
-            format!("^((?:≥\\s*)|(?:>=\\s*)|(?:=\\s*)|(?:<\\s*)|(?:=\\s*))v?{version}$")
-                .as_str(),
-        ).unwrap();
-        let hyphen = Regex::new(
-            format!("^[\\s]*{version}[\\s]*?\\s-\\s[\\s]*?{version}[\\s]*$").as_str(),
-        ).unwrap();
+            format!("^((?:≥\\s*)|(?:>=\\s*)|(?:=\\s*)|(?:<\\s*)|(?:=\\s*))v?{version}$").as_str(),
+        )
+        .unwrap();
+        let hyphen =
+            Regex::new(format!("^[\\s]*{version}[\\s]*?\\s-\\s[\\s]*?{version}[\\s]*$").as_str())
+                .unwrap();
 
         if semver.is_match(s) {
-            let cap = semver.captures(s)
+            let cap = semver
+                .captures(s)
                 .ok_or_else(|| anyhow!("Invalid version range: {}", s))?;
             VersionRange::parse_semver(s, cap)
         } else if inequality.is_match(s) {
-            let cap = inequality.captures(s)
+            let cap = inequality
+                .captures(s)
                 .ok_or_else(|| anyhow!("Invalid version range: {}", s))?;
             VersionRange::parse_inequality(s, cap)
         } else if hyphen.is_match(s) {
-            let cap = hyphen.captures(s)
+            let cap = hyphen
+                .captures(s)
                 .ok_or_else(|| anyhow!("Invalid version range: {}", s))?;
             VersionRange::parse_hyphen(s, cap)
         } else {
@@ -52,17 +55,26 @@ impl VersionRange {
             if v.major != 0 {
                 Ok(VersionRange::new(v, VersionBound::new((v.major, 0, 0), 1)))
             } else if v.minor != 0 {
-                Ok(VersionRange::new(v, VersionBound::new((v.major, v.minor, 0), 2)))
+                Ok(VersionRange::new(
+                    v,
+                    VersionBound::new((v.major, v.minor, 0), 2),
+                ))
             } else {
                 if v.n < 3 {
                     Ok(VersionRange::new(v, VersionBound::new((0, 0, 0), v.n)))
                 } else {
-                    Ok(VersionRange::new(v, VersionBound::new((0, 0, v.patch), v.n)))
+                    Ok(VersionRange::new(
+                        v,
+                        VersionBound::new((0, 0, v.patch), v.n),
+                    ))
                 }
             }
         } else if typ == "~" {
             if v.n == 3 || v.n == 2 {
-                Ok(VersionRange::new(v, VersionBound::new((v.major, v.minor, 0), 2)))
+                Ok(VersionRange::new(
+                    v,
+                    VersionBound::new((v.major, v.minor, 0), 2),
+                ))
             } else {
                 Ok(VersionRange::new(v, VersionBound::new((v.major, 0, 0), 1)))
             }
@@ -88,8 +100,8 @@ impl VersionRange {
             Ok(VersionRange::new(nil, upper))
         } else if Regex::new(r#"^=\s*$"#)?.is_match(typ) {
             Ok(VersionRange::new(v, v))
-        } else if Regex::new(r#"^>=\s*$"#)?.is_match(typ) ||
-            Regex::new(r#"^≥\s*$"#)?.is_match(typ) {
+        } else if Regex::new(r#"^>=\s*$"#)?.is_match(typ) || Regex::new(r#"^≥\s*$"#)?.is_match(typ)
+        {
             Ok(VersionRange::new(v, VersionBound::inf()))
         } else {
             anyhow::bail!("invalid prefix {typ}")
@@ -103,17 +115,11 @@ impl VersionRange {
             return Err(err());
         }
 
-        let lower_major = cap.get(1)
-            .ok_or_else(err)?
-            .as_str()
-            .parse::<u64>()?;
+        let lower_major = cap.get(1).ok_or_else(err)?.as_str().parse::<u64>()?;
         let lower_minor = cap.get(2);
         let lower_patch = cap.get(3);
 
-        let upper_major = cap.get(4)
-            .ok_or_else(err)?
-            .as_str()
-            .parse::<u64>()?;
+        let upper_major = cap.get(4).ok_or_else(err)?.as_str().parse::<u64>()?;
         let upper_minor = cap.get(5);
         let upper_patch = cap.get(6);
 
@@ -122,15 +128,13 @@ impl VersionRange {
                 let minor = minor.as_str().parse::<u64>()?;
                 let patch = patch.as_str().parse::<u64>()?;
                 VersionBound::new((lower_major, minor, patch), 3)
-            },
+            }
             (Some(minor), None) => {
                 let minor = minor.as_str().parse::<u64>()?;
                 VersionBound::new((lower_major, minor, 0), 2)
-            },
-            (None, None) => {
-                VersionBound::new((lower_major, 0, 0), 1)
             }
-            _ => unreachable!()
+            (None, None) => VersionBound::new((lower_major, 0, 0), 1),
+            _ => unreachable!(),
         };
 
         let upper = match (upper_minor, upper_patch) {
@@ -138,15 +142,13 @@ impl VersionRange {
                 let minor = minor.as_str().parse::<u64>()?;
                 let patch = patch.as_str().parse::<u64>()?;
                 VersionBound::new((upper_major, minor, patch), 3)
-            },
+            }
             (Some(minor), None) => {
                 let minor = minor.as_str().parse::<u64>()?;
                 VersionBound::new((upper_major, minor, 0), 2)
-            },
-            (None, None) => {
-                VersionBound::new((upper_major, 0, 0), 1)
             }
-            _ => unreachable!()
+            (None, None) => VersionBound::new((upper_major, 0, 0), 1),
+            _ => unreachable!(),
         };
 
         Ok(VersionRange::new(lower, upper))
@@ -158,17 +160,23 @@ impl VersionRange {
         }
 
         let n_significant = cap.iter().filter(|c| c.is_some()).count() - 1;
-        let typ = cap.get(1).ok_or_else(|| anyhow!("Invalid version range: {}", s))?.as_str();
-        let major = cap.get(2)
+        let typ = cap
+            .get(1)
             .ok_or_else(|| anyhow!("Invalid version range: {}", s))?
-            .as_str().parse::<u64>()?;
+            .as_str();
+        let major = cap
+            .get(2)
+            .ok_or_else(|| anyhow!("Invalid version range: {}", s))?
+            .as_str()
+            .parse::<u64>()?;
 
         let minor = if n_significant < 2 {
             0
         } else {
             cap.get(3)
                 .ok_or_else(|| anyhow!("Invalid version range: {}", s))?
-                .as_str().parse::<u64>()?
+                .as_str()
+                .parse::<u64>()?
         };
 
         let patch = if n_significant < 3 {
@@ -176,14 +184,18 @@ impl VersionRange {
         } else {
             cap.get(4)
                 .ok_or_else(|| anyhow!("Invalid version range: {}", s))?
-                .as_str().parse::<u64>()?
+                .as_str()
+                .parse::<u64>()?
         };
 
         if n_significant == 3 && major == 0 && minor == 0 && patch == 0 {
             anyhow::bail!("Invalid version range: 0.0.0");
         }
 
-        Ok((typ.into(), VersionBound::new((major, minor, patch), n_significant)))
+        Ok((
+            typ.into(),
+            VersionBound::new((major, minor, patch), n_significant),
+        ))
     }
 
     pub fn contains(&self, version: &Version) -> bool {
