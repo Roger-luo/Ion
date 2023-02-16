64280dad48e9c234ab3c76d0dd0fcfa44bcc12a4
diversable <dan.mantei@outlook.com>
2023-02-15 16:28:08 -0800

    test: update template tests

--- tests/bin/template.rs
+++ tests/bin/template.rs
@@ -1,7 +1,8 @@
 use super::*;
+use anyhow::{Ok, Result};
 
 #[test]
-fn test_template() {
+fn test_template() -> Result<()> {
     Ion::new().arg("template").arg("list").assert().success();
 
     Ion::new().arg("template").arg("update").assert().success();
@@ -19,4 +20,27 @@ fn test_template() {
         .arg("package")
         .assert()
         .success();
+
+    // Test escape from input / Ctrl+C behaviour with `ion template inspect` (cf. ask_inspect_input fn)
+    let mut p = Ion::new()
+        .arg("template")
+        .arg("inspect")
+        .arg("nonce")
+        .spawn(Some(30_000))?;
+
+    p.send_control('c')?;
+    p.exp_eof()?;
+
+    Ok(())?;
+
+    // Test nonce input
+    let mut ps = Ion::new()
+        .arg("template")
+        .arg("inspect")
+        .arg("nonce")
+        .spawn(Some(5_000))?;
+
+    ps.exp_string("Installed templates are:")?;
+
+    Ok(())
 }
