d728b96da795ecbf91b7965eb2a13e4c8b70a058
Rogerluo <rogerluo.rl18@gmail.com>
2023-02-21 12:01:16 -0500

    feat: rework CI scripts (#32)

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,16 @@
+use clap::Command;
+
+mod bump;
+
 fn main() -> Result<(), anyhow::Error> {
-    xtaskops::tasks::main()
+    let app = Command::new("xtask")
+        .about("A task runner for the xtask crate")
+        .subcommand(bump::cli())
+        .arg_required_else_help(true);
+    let matches = app.get_matches();
+
+    match matches.subcommand() {
+        Some(("bump", submatches)) => bump::exec(submatches),
+        _ => Ok(()),
+    }
 }
